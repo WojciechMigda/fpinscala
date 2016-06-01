f01ec3ddc22203e453591f16be0ad91b7fcd0b41
Wojciech Migda <wojtek.golf@interia.pl>
2016-06-01 22:16:18 +0200

    TestList

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -7,62 +7,98 @@ which may be `Nil` or another `Cons`.
  */
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
-object List { // `List` companion object. Contains functions for creating and working with lists.
-  def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
-    case Nil => 0 // The sum of the empty list is 0.
-    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
-  }
-
-  def product(ds: List[Double]): Double = ds match {
-    case Nil => 1.0
-    case Cons(0.0, _) => 0.0
-    case Cons(x,xs) => x * product(xs)
-  }
-
-  def apply[A](as: A*): List[A] = // Variadic function syntax
-    if (as.isEmpty) Nil
-    else Cons(as.head, apply(as.tail: _*))
-
-  val x = List(1,2,3,4,5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t) => h + sum(t)
-    case _ => 101
-  }
-
-  def append[A](a1: List[A], a2: List[A]): List[A] =
-    a1 match {
-      case Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
+object List
+{ // `List` companion object. Contains functions for creating and working with lists.
+    def sum(ints: List[Int]): Int = ints match
+    { // A function that uses pattern matching to add up a list of integers
+        case Nil => 0 // The sum of the empty list is 0.
+        case Cons(x, xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
     }
 
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
-    as match {
-      case Nil => z
-      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    def product(ds: List[Double]): Double = ds match
+    {
+        case Nil => 1.0
+        case Cons(0.0, _) => 0.0
+        case Cons(x, xs) => x * product(xs)
     }
 
-  def sum2(ns: List[Int]) =
-    foldRight(ns, 0)((x,y) => x + y)
+    def apply[A](as: A*): List[A] = // Variadic function syntax
+        if (as.isEmpty) Nil
+        else Cons(as.head, apply(as.tail: _*))
 
-  def product2(ns: List[Double]) =
-    foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
+    val x = List(1, 2, 3, 4, 5) match
+    {
+        case Cons(x, Cons(2, Cons(4, _))) => x
+        case Nil => 42
+        case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+        case Cons(h, t) => h + sum(t)
+        case _ => 101
+    }
 
+    def append[A](a1: List[A], a2: List[A]): List[A] =
+        a1 match {
+            case Nil => a2
+            case Cons(h, t) => Cons(h, append(t, a2))
+        }
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+    def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+        as match
+        {
+            case Nil => z
+            case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+        }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+    def sum2(ns: List[Int]) =
+        foldRight(ns, 0)((x, y) => x + y)
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+    def product2(ns: List[Double]) =
+        foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+    def tail[A](l: List[A]): List[A] =
+        l match
+        {
+            case Nil => l
+            case Cons(h, t) => t
+        }
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+    def setHead[A](l: List[A], h: A): List[A] =
+        l match
+        {
+            case Nil => l
+            case Cons(lh, lt) => Cons(h, lt)
+        }
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+    def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+    def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+    def init[A](l: List[A]): List[A] = sys.error("todo")
+
+    def length[A](l: List[A]): Int = sys.error("todo")
+
+    def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+
+    def map[A, B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+}
+
+object TestList
+{
+
+    import List._
+
+    // test implementation of `isSorted`
+    def main(args: Array[String]): Unit =
+    {
+        println("[tail] Expected: Nil")
+        println("[tail] Actual: %s\n".format(tail(Nil)))
+
+        println("[tail] Expected: Cons(2,Cons(3,Nil))")
+        println("[tail] Actual: %s\n".format(tail(List(1, 2, 3))))
+
+        println("[setHead] Expected: Nil")
+        println("[setHead] Actual: %s\n".format(setHead(Nil, 2)))
+
+        println("[setHead] Expected: Cons(4,Cons(2,Nil))")
+        println("[setHead] Actual: %s\n".format(setHead(List(1, 2), 4)))
+    }
 }
